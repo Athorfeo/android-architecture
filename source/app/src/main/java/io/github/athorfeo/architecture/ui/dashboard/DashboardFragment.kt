539e4a1b93d96683226491d4a2392c6a5b5127c8
Athorfeo <juanortizbayona@gmail.com>
2020-10-22 01:57:25 -0500

    - configuracion de estilos y recursos

--- source/app/src/main/java/io/github/athorfeo/architecture/ui/dashboard/DashboardFragment.kt
+++ source/app/src/main/java/io/github/athorfeo/architecture/ui/dashboard/DashboardFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -13,7 +14,7 @@ import io.github.athorfeo.architecture.ui.fragment.BaseFragment
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import timber.log.Timber
 
-class DashboardFragment: BaseFragment() {
+class DashboardFragment: BaseFragment(), View.OnClickListener {
     private lateinit var binding: FragmentDashboardBinding
     private val viewModel: DashboardViewModel by viewModels { viewModelFactory }
 
@@ -24,18 +25,31 @@ class DashboardFragment: BaseFragment() {
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         binding =  DataBindingUtil.inflate(layoutInflater, R.layout.fragment_dashboard, null, false)
+
+        with(binding){
+            clickListener = this@DashboardFragment
+        }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*viewModel.messages.observe(viewLifecycleOwner, Observer {
-            Timber.tag("ArchApp").i(it)
-        })*/
-
         viewModel.movies.observe(viewLifecycleOwner, Observer {
             Timber.tag("ArchApp").i(it.toString())
         })
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.button_light_theme -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            R.id.button_dark_theme -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
     }
 }
